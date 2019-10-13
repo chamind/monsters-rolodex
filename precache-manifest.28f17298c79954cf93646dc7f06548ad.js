ae6be99b810718b50ed4c87e7907234102c2b965
calmcham <calmcham@gmail.com>
2019-10-13 23:43:16 +0800

    Updates

---
+++ precache-manifest.28f17298c79954cf93646dc7f06548ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a42327abdf73734f7d682733e0a6f5a6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "53975e182554f80162a0",
+    "url": "/monsters-rolodex/static/css/main.e3562b14.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "53975e182554f80162a0",
+    "url": "/monsters-rolodex/static/js/main.ad8d7f53.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
